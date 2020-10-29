5e72cb07c54f02ca22604956a4ef4b1b91365a1a
加納 愼之典 <kano.s@impl.co.jp>
2020-10-29 21:36:48 +0900

    my first graphql

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
-import { Post } from './entities/Post';
 import mikroConfig from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolvers } from './resolvers/hello';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
@@ -9,14 +12,31 @@ const main = async () => {
   /* mikroConfigを元にマイグレーションする -> DB作成 */
   await orm.getMigrator().up();
 
+  /* Expressサーバーの作成 */
+  const app = express();
+
+  /* ApolloServerの作成 */
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolvers],
+      validate: false,
+    }),
+  });
+
+  /* ApolloServerのミドルウェアとしてExpressを登録 */
+  apolloServer.applyMiddleware({ app });
+
+  /* サーバーの起動 */
+  app.listen(4000, () => {
+    console.log('----------< サーバー起動: localhost:4000 >---------- ');
+  });
+
   /* Postを作成してinsertする */
   // const post = orm.em.create(Post, { title: 'my first post!' });
   // await orm.em.persistAndFlush(post);
 
   /* 全てのPOSTをクエリする */
-  const posts = await orm.em.find(Post, {});
-  console.log(posts);
-  console.table(posts);
+  // const posts = await orm.em.find(Post, {});
 };
 
 main().catch((err) => {
