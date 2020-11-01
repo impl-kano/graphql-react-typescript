a220428a8bbca0eda0e1f9de707c3df538620b5a
加納 愼之典 <kano.s@impl.co.jp>
2020-11-02 00:40:14 +0900

    add sesssion Authentication

--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,41 @@ import { buildSchema } from 'type-graphql';
 import { HelloResolvers } from './resolvers/hello';
 import { PostResolvers } from './resolvers/posts';
 import { UserResolvers } from './resolvers/user';
+import redis from 'redis';
+import session from 'express-session';
+import connectRedis from 'connect-redis';
+import { MyContext } from './types';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
 
   /* mikroConfigを元にマイグレーションする -> DB作成 */
-  await orm.getMigrator().up();
+  // await orm.getMigrator().up();
 
   /* Expressサーバーの作成 */
   const app = express();
+
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  app.use(
+    session({
+      name: 'testsessionforlearninggraphql',
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365,
+        httpOnly: true,
+        secure: __prod__, // coolie only works with https
+        sameSite: 'lax', // csrf
+      },
+      saveUninitialized: false,
+      secret: 'thisisatestforlearningperpose',
+      resave: false,
+    }),
+  );
 
   /* ApolloServerの作成 */
   const apolloServer = new ApolloServer({
@@ -24,7 +50,7 @@ const main = async () => {
       resolvers: [HelloResolvers, PostResolvers, UserResolvers],
       validate: false,
     }),
-    context: () => ({ em: orm.em }),
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
   });
 
   /* ApolloServerのミドルウェアとしてExpressを登録 */
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -5,11 +5,12 @@ import {
   InputType,
   Mutation,
   ObjectType,
+  Query,
   Resolver,
 } from 'type-graphql';
 import argon2 from 'argon2';
 import { User } from '../entities/User';
-import { MyContext } from 'src/types';
+import { MyContext } from '../types';
 
 @InputType()
 class UsernamePasswordInput {
@@ -40,10 +41,21 @@ class UserResponse {
 
 @Resolver()
 export class UserResolvers {
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() { req, em }: MyContext) {
+    // without session means not logged in...
+    if (!req.session!.userId) {
+      return null;
+    }
+    // logged in with valid session
+    const user = await em.findOne(User, { id: req.session!.userId });
+    return user;
+  }
+
   @Mutation(() => UserResponse)
   async register(
     @Arg('options') options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext,
+    @Ctx() { em, req }: MyContext,
   ): Promise<UserResponse> {
     if (options.username.length < 2) {
       return {
@@ -84,13 +96,17 @@ export class UserResolvers {
         };
       }
     }
+
+    // sava userId as session -> auto login after register
+    req.session!.userId = user.id;
+
     return { user };
   }
 
   @Mutation(() => UserResponse)
   async login(
     @Arg('options') options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext,
+    @Ctx() { em, req }: MyContext,
   ): Promise<UserResponse> {
     const user = await em.findOne(User, { username: options.username });
     if (!user) {
@@ -114,6 +130,10 @@ export class UserResolvers {
         ],
       };
     }
+
+    // save userid as session data...
+    req.session!.userId = user.id;
+
     return { user };
   }
 }
